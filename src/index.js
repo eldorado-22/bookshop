37ac7e24fb7bb5b74975c1c275d03cd608bde26f
eldorado-22 <jumashevv992@gmail.com>
2023-02-28 09:47:26 +0600

    header

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Layout from "./components/Layout/layout";
+import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <Layout>
-          <App />
-      </Layout>
+     <BrowserRouter>
+         <Layout>
+             <App />
+         </Layout>
+     </BrowserRouter>
   </React.StrictMode>
 );
 

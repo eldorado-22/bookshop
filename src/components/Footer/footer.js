27c6b10447e97100e4281cc51575b103be3c1747
Eldorado.JM <119120263+eldorado-22@users.noreply.github.com>
2023-02-28 18:11:55 +0600

    footer (#3)

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,10 +1,77 @@
 import React from 'react';
 import './footer.scss';
+import {NavLink} from "react-router-dom";
+import {BsFacebook, BsFillTelephoneFill, BsInstagram, BsLinkedin, BsTwitter} from "react-icons/bs";
+import {SiGmail} from "react-icons/si";
+import {BiMap} from "react-icons/bi";
+
 const Footer = () => {
     return (
-        <div>
+        <footer id="footer">
+            <div className="container">
+                <div className="footer">
 
-        </div>
+                    <div className="footer--sign-up">
+                        <h5>Sign up for our newsletter</h5>
+                        {/*<p>Be the first to know about our special offers, news, and updates.</p>*/}
+                        <div className="footer--sign-up__input">
+                            <input text="text" placeholder="Email Address"/>
+                            <p className="footer--sign-up__input--exit">Sign Up</p>
+                        </div>
+                    </div>
+
+                    <nav className="footer--home">
+                        <NavLink to={'/'}>
+                            <h5>Home</h5>
+                        </NavLink>
+                        <a href="#">Lorem</a>
+                        <a href="#">Lorem</a>
+                        <a href="#">Lorem</a>
+                        <a href="#">Lorem</a>
+                    </nav>
+
+                    <nav className="footer--categories">
+                        <NavLink to={'/categories'}>
+                            <h5>Categories</h5>
+                        </NavLink>
+                        <a href="#">Lorem</a>
+                        <a href="#">Lorem</a>
+                        <a href="#">Lorem</a>
+                        <a href="#">Lorem</a>
+                    </nav>
+
+
+                    <nav className="footer--contact">
+                        <h5>Contact</h5>
+                        <div className="footer--contact__ema-group">
+                            <a href="tel: 777 777 777" className="footer--contact__ema-group--phone">+(996) 777 777 777</a>
+                            <a href="Email" className="footer--contact__ema-group--email">motionweb@gmail.com</a>
+                        </div>
+
+
+                        <div className="footer--contact__social">
+                            <a href="https://www.instagram.com/motion_web/"
+                               className="footer--contact__social--instagram"><BsInstagram/></a>
+                            <a href="https://www.facebook.com/"
+                               className="footer--contact__social--facebook"><BsFacebook/></a>
+                            <a href="https://www.twitter.com/" className="footer--contact__social--twitter">
+                                <BsTwitter className="footer--contact__social--twitter__icon"/>
+                            </a>
+                        </div>
+
+                    </nav>
+
+                    <nav className="footer--address">
+                        <h2>Address</h2>
+                        <p>Kyrgyzstan Bishkek</p>
+                        <a className="footer--address__map" href="https://www.google.com/maps/place/Motion+Web/@42.8797851,74.5833385,17z/data=!3m1!4b1!4m6!3m5!1s0x389ec9fc655f0bb9:0x20d210594faeb18!8m2!3d42.8797851!4d74.5855272!16s%2Fg%2F11nf_2wmrw">
+                           <BiMap className="footer--address__map--location"/>
+                            Turusbekov: 109/3
+                        </a>
+                    </nav>
+                </div>
+            </div>
+        </footer>
     );
 };
 

27c066713797bb9c06e7e8e6e8ae0ce4deb3ddc8
bekzatWeb <118831768+bekzatWeb@users.noreply.github.com>
2023-02-28 15:07:02 +0300

    hero (#2)
    Co-authored-by: bekzat041982 <bekzatmurzalievn@gmail.com>

--- src/components/mainPage/Hero/hero.js
+++ src/components/mainPage/Hero/hero.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './hero.scss';
+
 const Hero = () => {
     return (
-        <div>
-
-        </div>
+        <section id="hero">
+            <div className="fon">
+                <div className="container">
+                    <div className="hero">
+                        <h1>Welcome to our Bookshop</h1>
+                        <p>A place where you can get all the books you want!</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 

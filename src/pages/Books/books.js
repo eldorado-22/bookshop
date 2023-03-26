368fa97320a2a95107a8494d1fadbe5baadce5f0
Tima0202 <enjoyhub02@gmail.com>
2023-03-26 13:50:16 +0600

    fdbresrdrgefasrdg

--- src/pages/Books/books.js
+++ src/pages/Books/books.js
@@ -4,24 +4,6 @@ import {NavLink} from "react-router-dom";
 import axios from "axios";
 
 
-// AIzaSyD9GZ2jDqvbYKavrV6dJw1B1rcTcR9OLe0
-
-const Books = () => {
-    const [books, setBooks] = useState([])
-
-    const getBooks=async ()=> {
-        try {
-            const res = await axios(`GET https://www.googleapis.com/books/v1/volumes?q=flowers&orderBy=newest&key=AIzaSyD9GZ2jDqvbYKavrV6dJw1B1rcTcR9OLe0`)
-           const {data} = await res
-           await setBooks(data)
-        }catch (e){
-            console.log(e, 'error')
-        }
-    }
-
-    useEffect(() => {
-      getBooks()
-
 const Books = () => {
 
     const [book, setBook] = useState([])
@@ -35,8 +17,7 @@ const Books = () => {
         getBooks()
     }, [])
 
-    console.log(books)
-    
+
     return (
         <div id="books">
             <div className="container">
@@ -46,17 +27,6 @@ const Books = () => {
                         <NavLink to={'/categories'}>View all</NavLink>
                     </div>
                     <div className="down">
-                        <Slider {...settings}>
-                            {
-                                books.map(el=>(
-                                    <div className='books-cart'>
-                                        <img src={el.image} alt="img"/>
-                                        <h3>{el.name}</h3>
-                                        <p>{el.desc}</p>
-                                    </div>
-                                ))
-                            }
-                        </Slider>
                         {
                             book.slice(0, 15).map(el => (
                                 <div className='cart'>
